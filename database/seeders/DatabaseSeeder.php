8c800f3a67ce9b8ccf87ee8504a9c17c910ef78a
ACH RIZAL <achrizal15@gmail.com>
2021-12-11 22:39:29 +0700

    UPDATE

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,24 +22,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([RoleSeeder::class]);
 
-        User::factory(100)->create();
-        Package::factory(5)->create();
-        Inventory::factory(100)->create();
-        Expenditure::factory(100)->create();
-        Blok::create([
-            "name"=>"BLOK A",
-            "detail_address"=>"barat",
-            "collector_id"=>"15",
-        ]);
-        Blok::create([
-            "name"=>"BLOK B",
-            "detail_address"=>"timur",
-            "collector_id"=>"12",
-        ]);
-        Blok::create([
-            "name"=>"BLOK C",
-            "detail_address"=>"selatan",
-            "collector_id"=>"5",
-        ]);
+        User::factory(10)->create();
+        Package::factory(3)->create();
+        Inventory::factory(50)->create();
+        Expenditure::factory(50)->create();     
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,6 @@
 <body>
     Ini adalah landing page 
     <a href="/login">Login</a>
+    <a href="https://php.id/ref/31313" class="absolute bottom-0 left-0 right-0" target="_blank"><img src="/storage/phpid.png"></a>
 </body>
 @endsection
